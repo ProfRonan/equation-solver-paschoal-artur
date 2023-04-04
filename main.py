38e25161a3a8c4a73de720ea9925c3ff3175f37f
paschoal-artur <paschoal.artur@alu.ufc.br>
2023-04-03 22:05:17 -0300

    added code2

--- main.py
+++ main.py
@@ -4,29 +4,31 @@ Dg = int(input("Informe um grau de equação: \n"))
 #Operating with the information
 if Dg > 2 or Dg < 1 :
     print("Grau inválido")
+    exit()
 if Dg == 1 :
     print("A equação é do primeiro grau")
-    auser = int(input("Informe um valor para o 'a' da equação: \n"))
+    auser = float(input("Informe um valor para o 'a' da equação: \n"))
     if auser == 0:
         print("Valor de 'a' inválido")
+        exit()
     if auser != 0 :
-        buser = int(input("Informe um valor para o 'b' da equação : \n"))
-        operation = x = -buser/auser
-        operation = int(operation)
-        print(f"A raíz da sua equação é {operation}")
+        buser = float(input("Informe um valor para o 'b' da equação : \n"))
+        operation = float(-buser)/auser
+        print(f"A raíz da sua equação é {round(operation, 2)}")
         exit()
 if Dg == 2 :
     print("A equação é do segundo grau")
-    auser = int(input("Informe um valor para o 'a' da equação: \n"))
-    if auser == 0:
+    auser2 = float(input("Informe um valor para o 'a' da equação: \n"))
+    if auser2 == 0:
         print("Valor de a inválido")
-if auser != 0 :
-    buser = int(input("Informe um valor para o 'b' da equação : \n"))
-    cuser = int(input("Informe um valor para o 'c' da equação : \n"))
-    delta = int((buser**2) - 4 * auser * cuser)
-    if delta < 0 :
+        exit()
+    if auser2 != 0 :
+        buser2 = float(input("Informe um valor para o 'b' da equação : \n"))
+        cuser2 = float(input("Informe um valor para o 'c' da equação : \n"))
+        delta2 = float((buser2**2) - 4 * auser2 * cuser2)
+    if delta2 < 0 :
         print("A equação não possui raízes reais")
-    if delta == 0 :
-        print("A equação possui uma raíz real")
-    if delta > 0 :
+    if delta2 == 0 :
+        print("A equação possui uma raiz real")
+    if delta2 > 0 :
         print("A equação possui duas raízes reais")
