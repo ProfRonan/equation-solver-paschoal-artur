798796cd68d3bb2f46bbec052a9f2c600f7951f6
paschoal-artur <paschoal.artur@alu.ufc.br>
2023-04-03 22:41:01 -0300

    updated code7

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #Receiving information from user
-Dg = int(input("Informe um grau de equação: \n"))
+Dg = int(input("Informe um grau de equação: "))
 
 #Operating with the information
 if Dg > 2 or Dg < 1 :
@@ -7,24 +7,24 @@ if Dg > 2 or Dg < 1 :
 
 if Dg == 1 :
     print("A equação é do primeiro grau")
-    auser = float(input("Informe um valor para o 'a' da equação: \n"))
+    auser = float(input("Informe um valor para o 'a' da equação: "))
     if auser == 0:
         print("Valor de a inválido")
 
     if auser != 0 :
-        buser = float(input("Informe um valor para o 'b' da equação : \n"))
+        buser = float(input("Informe um valor para o 'b' da equação : "))
         operation = float((-buser)/auser)
         print(f"A raíz da sua equação é {operation: .2f}")
 
 if Dg == 2 :
     print("A equação é do segundo grau")
-    auser2 = float(input("Informe um valor para o 'a' da equação: \n"))
+    auser2 = float(input("Informe um valor para o 'a' da equação: "))
     if auser2 == 0:
         print("Valor de a inválido")
 
     if auser2 != 0 :
-        buser2 = float(input("Informe um valor para o 'b' da equação : \n"))
-        cuser2 = float(input("Informe um valor para o 'c' da equação : \n"))
+        buser2 = float(input("Informe um valor para o 'b' da equação : "))
+        cuser2 = float(input("Informe um valor para o 'c' da equação : "))
         delta2 = float((buser2**2) - 4 * (auser2 * cuser2))
     if delta2 < 0 :
         print("A equação não possui raízes reais")
