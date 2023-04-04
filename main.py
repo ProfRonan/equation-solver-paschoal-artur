34feea14f933cdf8ce6b6437592b3813ae9428fb
paschoal-artur <paschoal.artur@alu.ufc.br>
2023-04-03 22:38:53 -0300

    updated code6

--- main.py
+++ main.py
@@ -4,33 +4,33 @@ Dg = int(input("Informe um grau de equação: \n"))
 #Operating with the information
 if Dg > 2 or Dg < 1 :
     print("Grau inválido")
-    exit()
+
 if Dg == 1 :
     print("A equação é do primeiro grau")
     auser = float(input("Informe um valor para o 'a' da equação: \n"))
     if auser == 0:
         print("Valor de a inválido")
-        exit()
+
     if auser != 0 :
         buser = float(input("Informe um valor para o 'b' da equação : \n"))
         operation = float((-buser)/auser)
         print(f"A raíz da sua equação é {operation: .2f}")
-        exit()
+
 if Dg == 2 :
     print("A equação é do segundo grau")
     auser2 = float(input("Informe um valor para o 'a' da equação: \n"))
     if auser2 == 0:
         print("Valor de a inválido")
-        exit()
+
     if auser2 != 0 :
         buser2 = float(input("Informe um valor para o 'b' da equação : \n"))
         cuser2 = float(input("Informe um valor para o 'c' da equação : \n"))
         delta2 = float((buser2**2) - 4 * (auser2 * cuser2))
     if delta2 < 0 :
         print("A equação não possui raízes reais")
-        exit()
+
     if delta2 == 0 :
         print("A equação possui apenas uma raiz real")
-        exit()
+
     if delta2 > 0 :
         print("A equação possui duas raízes reais")
