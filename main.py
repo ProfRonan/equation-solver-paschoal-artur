b93764f398bab7e9a893321177ebfbd246106560
paschoal-artur <paschoal.artur@alu.ufc.br>
2023-04-03 23:18:24 -0300

    updated code12

--- main.py
+++ main.py
@@ -4,36 +4,36 @@ Dg = int(input("Informe o grau da equação: "))
 #Operating with the information
 if Dg > 2 or Dg < 1 :
     print("Grau inválido")
-    exit()
+
 if Dg == 1 :
     print("A equação é do primeiro grau")
     auser = int(input("Informe um valor para o 'a' da equação: "))
     if auser == 0:
         print("Valor de a inválido")
-        exit()
+
     if auser != 0 :
         buser = int(input("Informe um valor para o 'b' da equação : "))
         operation = int((-buser)/auser)
         print(f"{operation: .2f}")
-        exit()
+
 if Dg == 2 :
     print("A equação é do segundo grau")
     auser2 = int(input("Informe um valor para o 'a' da equação: "))
     if auser2 == 0:
         print("Valor de a inválido")
-        exit()
+
     if auser2 != 0 :
         buser2 = int(input("Informe um valor para o 'b' da equação : "))
         cuser2 = int(input("Informe um valor para o 'c' da equação : "))
         delta = int((buser2**2) - 4 * (auser2 * cuser2))
     if delta < 0 :
         print("A equação não possui raízes reais")
-        exit()
+
     if delta == 0 :
         print("A equação possui apenas uma raiz real")
         root_1 = int(-buser2 / 2 * auser2)
         print(f"{root_1: .2f}")
-        exit()
+
     if delta > 0 :
         print("A equação possui duas raízes reais")
         root_positive = -(buser2) + (delta **(1/2)) / 2 * auser2
